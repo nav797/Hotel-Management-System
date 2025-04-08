d3de94c163b1cf985bcdf89a897f0d50b42fdf29
nav797 <navneetdhillon79@gmail.com>
2025-04-07 19:03:20 -0700

    Admin Page and User Factory

---
+++ src/main/java/project/AdminPage.java
@@ -0,0 +1,59 @@
+package project;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+
+public class AdminPage extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminPage frame = new AdminPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 750, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Admin Console");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblNewLabel.setBounds(37, 35, 140, 16);
+		contentPane.add(lblNewLabel);
+		
+		JButton userButton = new JButton("Create Users");
+		userButton.setBounds(28, 64, 117, 29);
+		contentPane.add(userButton);
+		
+		JButton reportButton = new JButton("Reports");
+		reportButton.setBounds(28, 105, 117, 29);
+		contentPane.add(reportButton);
+	}
+
+}
---
+++ src/main/java/project/UserFactory.java
@@ -0,0 +1,173 @@
+package project;
+
+
+	interface User {
+		
+		 String info();
+		}
+	
+	class Guest implements User {
+		private String username;
+		private String firstName;
+		private String lastName;
+		private String password;
+		
+		private String role = "Guest";
+		
+	 public Guest(String u, String f, String l, String p) {
+			username = u;
+			firstName = f;
+			lastName = l;
+			password = p;
+		}
+		
+		public String getUsername() {
+			return username;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public String getRole() {
+			return role;
+		}
+	
+	 public String info() {
+	 return role+" User"+firstName+" "+lastName+" created!";
+	 }
+	}
+	
+	
+	
+	class Housekeeping implements User {
+		private String username;
+		private String firstName;
+		private String lastName;
+		private String password;
+		
+		private String role = "Housekeeping";
+		
+	 public Housekeeping(String u, String f, String l, String p) {
+			username = u;
+			firstName = f;
+			lastName = l;
+			password = p;
+		}
+		
+		public String getUsername() {
+			return username;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public String getRole() {
+			return role;
+		}
+	
+	 public String info() {
+	 return role+" User"+firstName+" "+lastName+" created!";
+	 }
+	}
+	
+	
+	class Receptionist implements User {
+		private String username;
+		private String firstName;
+		private String lastName;
+		private String password;
+		
+		private String role = "Receptionist";
+		
+	 public Receptionist(String u, String f, String l, String p) {
+			username = u;
+			firstName = f;
+			lastName = l;
+			password = p;
+		}
+		
+		public String getUsername() {
+			return username;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public String getRole() {
+			return role;
+		}
+	
+	 public String info() {
+	 return role+" User"+firstName+" "+lastName+" created!";
+	 }
+	}
+	
+	class Admin implements User {
+		private String username;
+		private String firstName;
+		private String lastName;
+		private String password;
+		
+		private String role = "Admin";
+		
+	 public Admin(String u, String f, String l, String p) {
+			username = u;
+			firstName = f;
+			lastName = l;
+			password = p;
+		}
+		
+		public String getUsername() {
+			return username;
+		}
+		public String getFirstName() {
+			return firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public String getRole() {
+			return role;
+		}
+	
+	 public String info() {
+	 return role+" User"+firstName+" "+lastName+" created!";
+	 }
+	}
+	
+	
+	public class UserFactory {
+	 public static User createUser(String u, String f, String l, String p, String r) {
+	 switch (r) {
+	 case "Guest":
+		 return new Guest(u,f,l,p);
+	 case "Housekeeping":
+		 return new Housekeeping(u,f,l,p);
+	 case "Receptionist":
+		 return new Receptionist(u,f,l,p);
+	 case "Admin":
+		 return new Admin(u,f,l,p);
+	 default:
+		 throw new IllegalArgumentException("Unknown role: " + r);
+	 }
+	 }
+	}
+
